c9239ce892319a696657c41dc39cd8d62914e794
gloriatampuri <gloria.tampuri@amalitech.org>
2023-09-19 14:23:49 +0000

    checking for previous week

--- components/Dashboard/WeeklyCashups/WeeklyCashups.js
+++ components/Dashboard/WeeklyCashups/WeeklyCashups.js
@@ -3,11 +3,11 @@ import useSWR from 'swr'
 import CashUp from '../CashUp/CashUp'
 import Link from 'next/link'
 import classes from './WeeklyCashups.module.css'
-
+import { useRouter } from 'next/router'
 const fetcher = (...args) => fetch(...args).then(res => res.json())
 
 const WeeklyCashups = () => {
-
+  const router=useRouter()
   const { data, error, isLoading } = useSWR('/api/asserts', fetcher);
 
   const allAssets = data?.asserts;
@@ -28,6 +28,13 @@ const WeeklyCashups = () => {
       (currentDate - new Date(currentDate.getFullYear(), 0, 1)) / 604800000
     ); // Calculate the current week number
   
+    // Calculate the previous week number
+const previousWeekStartDate = new Date(currentDate);
+previousWeekStartDate.setDate(previousWeekStartDate.getDate() - 7); // Subtract 7 days to get to the previous week's start date
+const previousWeek = Math.ceil(
+  (previousWeekStartDate - new Date(previousWeekStartDate.getFullYear(), 0, 1)) / 604800000
+)
+
     documents?.forEach((document) => {
       let totalSales = 0;
       document?.cashup?.forEach((sale) => {
@@ -52,6 +59,8 @@ const WeeklyCashups = () => {
     return documents;
   };
   
+ 
+  
 
 const cashUpWeeklyTable = calculateArrangeTotalSales(allAssets)
 
@@ -70,7 +79,7 @@ const cashUpWeeklyTable = calculateArrangeTotalSales(allAssets)
           </thead>
 
           <tbody>
-          {cashUpWeeklyTable?.map((arranged, index) => <tr key={arranged?._id}>
+          {cashUpWeeklyTable?.map((arranged, index) => <tr key={arranged?._id} onClick={()=>router.push(`/dashboard/asserts/${arranged?._id}/cashup `)} >
               <td>{index + 1}</td>
               <td className={classes.color}>{arranged?.location.find((val) => val?.currentLocation === true)?.locationName}</td>
               <td>{arranged?.assertId}</td>
@@ -86,3 +95,110 @@ const cashUpWeeklyTable = calculateArrangeTotalSales(allAssets)
 }
 
 export default WeeklyCashups
+
+
+// import React, { useState, useEffect } from 'react';
+// import useSWR from 'swr';
+// import CashUp from '../CashUp/CashUp';
+// import Link from 'next/link';
+// import classes from './WeeklyCashups.module.css';
+
+// const fetcher = (...args) => fetch(...args).then(res => res.json());
+
+// const WeeklyCashups = () => {
+//   const { data, error, isLoading } = useSWR('/api/asserts', fetcher);
+//   const allAssets = data?.asserts;
+
+//   function getWeekNumber(date) {
+//     const currentDate = date || new Date();
+//     const startDate = new Date(currentDate.getFullYear(), 0, 1); // January 1st of the current year
+//     const days = Math.floor((currentDate - startDate) / (24 * 60 * 60 * 1000));
+//     const weekNumber = Math.ceil((days + 1) / 7);
+
+//     return weekNumber;
+//   }
+
+//   const calculateArrangeTotalSales = (documents, currentDate) => {
+//     const currentYear = currentDate.getFullYear();
+
+//     // Calculate the start and end dates for the current week (from 11th September to 17th September 2023)
+//     const currentWeekStart = new Date(2023, 8, 11); // September is 8 (0-based index)
+//     const currentWeekEnd = new Date(2023, 8, 17); // End of September 17th
+
+//     const filteredDocuments = documents?.filter((document) => {
+//       return document.cashup.some((sale) => {
+//         const saleDate = new Date(sale.cashupDate);
+//         const saleYear = saleDate.getFullYear();
+
+//         return (
+//           saleYear === currentYear &&
+//           saleDate >= currentWeekStart &&
+//           saleDate <= currentWeekEnd
+//         );
+//       });
+//     });
+
+//     const arrangedDocuments = filteredDocuments?.map((document) => {
+//       let totalSales = 0;
+
+//       document.cashup.forEach((sale) => {
+//         const saleDate = new Date(sale.cashupDate);
+//         const saleYear = saleDate.getFullYear();
+
+//         if (
+//           saleYear === currentYear &&
+//           saleDate >= currentWeekStart &&
+//           saleDate <= currentWeekEnd
+//         ) {
+//           totalSales += sale.companyAmount;
+//         }
+//       });
+
+//       return {
+//         ...document,
+//         totalSalesCurrentWeek: totalSales || 0,
+//       };
+//     });
+
+//     // Sort documents based on the total sales in the current week
+//     arrangedDocuments?.sort((a, b) => b.totalSalesCurrentWeek - a.totalSalesCurrentWeek);
+
+//     return arrangedDocuments;
+//   };
+
+//   const currentDate = new Date();
+//   const cashUpWeeklyTable = calculateArrangeTotalSales(allAssets, currentDate);
+
+//   return (
+//     <div>
+//       <div className={classes.list}>
+//         <h2 className={classes.tabheader}>Performance of all assets for the week of September 11th to September 17th, 2023</h2>
+//         <table>
+//           <thead>
+//             <tr>
+//               <th>Position</th>
+//               <th>Location</th>
+//               <th>AssetID</th>
+//               <th>Cashup Amount</th>
+//             </tr>
+//           </thead>
+
+//           <tbody>
+//             {cashUpWeeklyTable?.map((arranged, index) => (
+//               <tr key={arranged?._id}>
+//                 <td>{index + 1}</td>
+//                 <td className={classes.color}>{arranged?.location.find((val) => val?.currentLocation === true)?.locationName}</td>
+//                 <td>{arranged?.assertId}</td>
+//                 <td className={classes.color}>{arranged?.totalSalesCurrentWeek}</td>
+//               </tr>
+//             ))}
+//           </tbody>
+//         </table>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default WeeklyCashups;
+
+
